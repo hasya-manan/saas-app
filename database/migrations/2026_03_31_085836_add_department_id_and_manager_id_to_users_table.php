9d8ea96635ec8ced53da0142e4a8ae9aca34fa59
hasya <hasyamanan@gmail.com>
2026-03-31 16:59:46 +0800

    modify users table :: add manager id and dept id

---
+++ database/migrations/2026_03_31_085836_add_department_id_and_manager_id_to_users_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            //
+            $table->unsignedBigInteger('department_id')->nullable()->after('tenant_id');
+            $table->unsignedBigInteger('manager_id')->nullable()->after('department_id');
+
+            $table->foreign('department_id')->references('id')->on('departments')->onDelete('set null');
+            $table->foreign('manager_id')->references('id')->on('users')->onDelete('set null');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            //
+            $table->dropForeign(['department_id']);
+            $table->dropForeign(['manager_id']);
+            $table->dropColumn(['department_id', 'manager_id']);
+        });
+    }
+};

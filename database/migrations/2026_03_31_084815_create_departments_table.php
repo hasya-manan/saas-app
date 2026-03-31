96cfd548c9a915e2b965d888b704be8a748a7fe2
hasya <hasyamanan@gmail.com>
2026-03-31 16:51:36 +0800

    Create:: migartion file departments table

---
+++ database/migrations/2026_03_31_084815_create_departments_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('departments', function (Blueprint $table) {
+            $table->id();
+            $table->string('tenant_id');
+            $table->string('name');
+            $table->text('description')->nullable();
+            $table->unsignedBigInteger('manager_id')->nullable();
+            $table->timestamps();
+
+            $table->foreign('tenant_id')->references('id')->on('tenants')->onDelete('cascade');
+            $table->foreign('manager_id')->references('id')->on('users')->onDelete('set null');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('departments');
+    }
+};

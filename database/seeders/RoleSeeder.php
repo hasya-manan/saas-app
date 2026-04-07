f565d34834da4ae00a2428ad0d2579d45643e2cf
hasya <hasyamanan@gmail.com>
2026-04-07 17:01:13 +0800

    Add :: change roleSeeder

--- database/seeders/RoleSeeder.php
+++ database/seeders/RoleSeeder.php
@@ -11,9 +11,18 @@ class RoleSeeder extends Seeder
      * Run the database seeds.
      */
     public function run(): void
-    {
-        \App\Models\Role::firstOrCreate(['name' => 'superadmin']);
-        \App\Models\Role::firstOrCreate(['name' => 'admin_company']);
-        \App\Models\Role::firstOrCreate(['name' => 'staff']);
+{
+    $roles = [
+        ['name' => 'superadmin', 'display_name' => 'System Administrator'],
+        ['name' => 'admin_company', 'display_name' => 'Company Admin'],
+        ['name' => 'staff', 'display_name' => 'Staff'],
+    ];
+
+    foreach ($roles as $role) {
+        \App\Models\Role::updateOrCreate(
+            ['name' => $role['name']], // Search criteria
+            ['display_name' => $role['display_name']] // Values to update/create
+        );
     }
+}
 }

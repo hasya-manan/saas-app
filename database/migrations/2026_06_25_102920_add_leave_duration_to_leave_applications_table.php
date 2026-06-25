b3beab02c6c8196691669cee15654592d372f208
hasya <hasyamanan@gmail.com>
2026-06-25 10:30:03 +0800

    add migration

---
+++ database/migrations/2026_06_25_102920_add_leave_duration_to_leave_applications_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('leave_applications', function (Blueprint $table) {
+            //full/ half
+            $table->string('leave_duration')->default('full')->after('end_date');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('leave_applications', function (Blueprint $table) {
+            //
+            $table->dropColumn('leave_duration');
+        });
+    }
+};
